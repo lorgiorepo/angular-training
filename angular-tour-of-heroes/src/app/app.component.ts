382b8814434ca508f53b10318c436717596ba298
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2017-09-09 18:11:55 -0300

    Render for hero object

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+      id: 1,
+      name: 'Windstorm'
+  };
 }
